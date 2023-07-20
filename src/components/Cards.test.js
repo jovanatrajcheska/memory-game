063b7c472c3887df27365ca95e9849d11f4def49
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-20 11:37:09 +0200

    Tests added

---
+++ src/components/Cards.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Card from './Card';
+
+describe('Card Component', () => {
+
+  const item = { id: 7, img: '/img/java.png', stat: '' };
+  const id = 0;
+  const handleClick = jest.fn();
+
+  test('without errors', () => {
+    render(<Card item={item} id={id} handleClick={handleClick} />);
+  });
+
+  test('when clicked', () => {
+    const { container } = render(<Card item={item} id={id} handleClick={handleClick} />);
+    fireEvent.click(container.firstChild);
+    expect(handleClick).toHaveBeenCalledWith(id);
+  });
+
+  test('card image', () => {
+    const { getByAltText } = render(<Card item={item} id={id} handleClick={handleClick} />);
+    const imgElem = getByAltText('');
+    expect(imgElem).toBeInTheDocument();
+    expect(imgElem.src).toContain(item.img);
+  });
+});

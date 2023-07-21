7c1212d3dfb8d25c83b6524d2e83967fca5dc24b
jovanatrajcheska <trajceskaj55@gmail.com>
2023-07-21 11:54:29 +0200

    Update Cards.js

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,84 +1,58 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
-import htmlImage from '../img/html.png';
-import cssImage from '../img/css.png';
-import jsImage from '../img/js.png';
-import sassImage from '../img/sass.png';
-import reactImage from '../img/react.png';
-import figmaImage from '../img/figma.png';
-import javaImage from '../img/java.png';
-import pythonImage from '../img/python.png';
 
-function Cards() {
+function Cards(props) {
 
     
-  const [items, setItems] = useState(() => {
-    const itemImgs = [
-      { id: 1, img: htmlImage, stat: '' },
-      { id: 1, img: htmlImage, stat: '' },
-      { id: 2, img: cssImage, stat: '' },
-      { id: 2, img: cssImage, stat: '' },
-      { id: 3, img: jsImage, stat: '' },
-      { id: 3, img: jsImage, stat: '' },
-      { id: 4, img: sassImage, stat: '' },
-      { id: 4, img: sassImage, stat: '' },
-      { id: 5, img: reactImage, stat: '' },
-      { id: 5, img: reactImage, stat: '' },
-      { id: 6, img: figmaImage, stat: '' },
-      { id: 6, img: figmaImage, stat: '' },
-      { id: 7, img: javaImage, stat: '' },
-      { id: 7, img: javaImage, stat: '' },
-      { id: 8, img: pythonImage, stat: '' },
-      { id: 8, img: pythonImage, stat: '' },
-    ];
-  
-    return itemImgs.sort(() => Math.random() - 0.5);
-  });
+    const [items, setItems] = useState([]);
+    const [prev, setPrev] = useState(-1);
 
-  const [prev, setPrev] = useState(-1);
+    useEffect(() => {
+        setItems(props.itemImgs.sort(() => Math.random() - 0.5));
+    }, [props.itemImgs]);
 
 
-  const check = (current) => {
-    if (items[current].id === items[prev].id) {
-      const updatedItems = [...items];
-      updatedItems[current].stat = 'correct';
-      updatedItems[prev].stat = 'correct';
-      setItems(updatedItems);
-      setPrev(-1);
-    } else {
-      const updatedItems = [...items];
-      updatedItems[current].stat = 'wrong';
-      updatedItems[prev].stat = 'wrong';
-      setItems(updatedItems);
+    const check = (current) => {
+        if (items[current].id === items[prev].id) {
+            const updatedItems = [...items];
+            updatedItems[current].stat = 'correct';
+            updatedItems[prev].stat = 'correct';
+            setItems(updatedItems);
+            setPrev(-1);
+        } else {
+            const updatedItems = [...items];
+            updatedItems[current].stat = 'wrong';
+            updatedItems[prev].stat = 'wrong';
+            setItems(updatedItems);
 
-      setTimeout(() => {
-        const resetItems = [...items];
-        resetItems[current].stat = '';
-        resetItems[prev].stat = '';
-        setItems(resetItems);
-        setPrev(-1);
-      }, 1000);
-    }
-  };
+            setTimeout(() => {
+                const resetItems = [...updatedItems];
+                resetItems[current].stat = '';
+                resetItems[prev].stat = '';
+                setItems(resetItems);
+                setPrev(-1);
+            }, 1000);
+        }
+    };
 
-  const functionClick = (id) => {
-    if (prev === -1) {
-      const updatedItems = [...items];
-      updatedItems[id].stat = 'active';
-      setItems(updatedItems);
-      setPrev(id);
-    } else {
-      check(id);
-    }
-  };
+    const functionClick = (id) => {
+        if (prev === -1) {
+            const updatedItems = [...items];
+            updatedItems[id].stat = 'active';
+            setItems(updatedItems);
+            setPrev(id);
+        } else {
+            check(id);
+        }
+    };
 
-  return (
-    <div className="container">
-      {items.map((item, index) => (
-        <Card key={index} item={item} id={index} handleClick={functionClick} />
-      ))}
-    </div>
-  );
+    return (
+        <div className="container">
+            {items.map((item, index) => (
+                <Card key={index} item={item} id={index} handleClick={functionClick} />
+            ))}
+        </div>
+    );
 }
 
 export default Cards;

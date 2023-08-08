a547f3b972e03377bb63fb04b9f4f60ee09700fc
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-08 16:32:10 +0200

    Modify Cards Component

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,57 +2,72 @@ import React, { useState, useEffect } from 'react';
 import Card from './Card';
 
 function Cards(props) {
+  const [items, setItems] = useState([]);
+  const [prev, setPrev] = useState(-1);
+  const [isChecking, setIsChecking] = useState(false);
 
-    
-    const [items, setItems] = useState([]);
-    const [prev, setPrev] = useState(-1);
+  useEffect(() => {
+    if (Array.isArray(props.itemImgs)) {
+      const shuffledItems = [...props.itemImgs].sort(() => Math.random() - 0.5);
+      setItems(shuffledItems);
+    }
+  }, [props.itemImgs]);
 
-    useEffect(() => {
-        setItems(props.itemImgs.sort(() => Math.random() - 0.5));
-    }, [props.itemImgs]);
+  const check = (current) => {
+    setIsChecking(true);
 
+    if (items[current].id === items[prev].id) {
+      const updatedItems = [...items];
+      updatedItems[current].stat = 'correct';
+      updatedItems[prev].stat = 'correct';
+      setItems(updatedItems);
+      setPrev(-1);
 
-    const check = (current) => {
-        if (items[current].id === items[prev].id) {
-            const updatedItems = [...items];
-            updatedItems[current].stat = 'correct';
-            updatedItems[prev].stat = 'correct';
-            setItems(updatedItems);
-            setPrev(-1);
-        } else {
-            const updatedItems = [...items];
-            updatedItems[current].stat = 'wrong';
-            updatedItems[prev].stat = 'wrong';
-            setItems(updatedItems);
+      // Allow further clicks immediately after a correct match
+      setIsChecking(false);
+    } else {
+      const updatedItems = [...items];
+      updatedItems[current].stat = 'wrong';
+      updatedItems[prev].stat = 'wrong';
+      setItems(updatedItems);
 
-            setTimeout(() => {
-                const resetItems = [...updatedItems];
-                resetItems[current].stat = '';
-                resetItems[prev].stat = '';
-                setItems(resetItems);
-                setPrev(-1);
-            }, 1000);
-        }
-    };
+      setTimeout(() => {
+        const resetItems = [...updatedItems];
+        resetItems[current].stat = '';
+        resetItems[prev].stat = '';
+        setItems(resetItems);
+        setPrev(-1);
+        setIsChecking(false);
+      }, 1000);
+    }
+  };
 
-    const functionClick = (id) => {
-        if (prev === -1) {
-            const updatedItems = [...items];
-            updatedItems[id].stat = 'active';
-            setItems(updatedItems);
-            setPrev(id);
-        } else {
-            check(id);
-        }
-    };
+  const functionClick = (id) => {
+    if (!isChecking && prev !== id && items[id].stat !== 'correct') {
+      if (prev === -1) {
+        const updatedItems = [...items];
+        updatedItems[id].stat = 'active';
+        setItems(updatedItems);
+        setPrev(id);
+      } else {
+        check(id);
+      }
+    }
+  };
 
-    return (
-        <div className="container">
-            {items.map((item, index) => (
-                <Card key={index} item={item} id={index} handleClick={functionClick} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="container" data-testid="cards-container">
+      {items.map((item, index) => (
+        <Card
+          key={index}
+          item={item}
+          id={index}
+          handleClick={functionClick}
+          isChecking={isChecking}
+        />
+      ))}
+    </div>
+  );
 }
 
 export default Cards;

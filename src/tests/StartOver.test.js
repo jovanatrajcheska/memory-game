bea74995b0a61c3886baa9e7f5d9795e1d9f44f3
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-14 00:08:08 +0200

    Add test for StartOver component

--- src/tests/StartOver.test.js
+++ src/tests/StartOver.test.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { render, fireEvent } from "@testing-library/react";
 import StartOver from "../components/StartOver";
-describe("StartOver", () => {
-  it("onRestart function is called when button is clicked", () => {
+
+describe("StartOver component", () => {
+
+  test("No errors detected", () => {
+    render(<StartOver />);
+  });
+
+  test("onRestart function is called when button is clicked", () => {
     const mockOnRestart = jest.fn();
     const { getByText } = render(<StartOver onRestart={mockOnRestart} />);
 
-    const startOverButton = getByText("Start Over");
-    fireEvent.click(startOverButton);
+    const startOverBtn = getByText("Start Over");
+    fireEvent.click(startOverBtn);
 
     expect(mockOnRestart).toHaveBeenCalled();
   });
+
 });

f5715010fafbaf040bfe887102d16a4b788614e0
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-14 00:02:58 +0200

    IDs changed for images

--- src/components/CardsProp.js
+++ src/components/CardsProp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Cards from './Cards';
+import Cards from '../screens/Cards';
 import htmlImage from '../img/html.png';
 import cssImage from '../img/css.png';
 import jsImage from '../img/js.png';
@@ -11,22 +11,22 @@ import pythonImage from '../img/python.png';
 
 const CardsProp = () => {
   const itemsImg = [
-    { id: 1, img: htmlImage, stat: '' },
-    { id: 1, img: htmlImage, stat: '' },
-    { id: 2, img: cssImage, stat: '' },
-    { id: 2, img: cssImage, stat: '' },
-    { id: 3, img: jsImage, stat: '' },
-    { id: 3, img: jsImage, stat: '' },
-    { id: 4, img: sassImage, stat: '' },
-    { id: 4, img: sassImage, stat: '' },
-    { id: 5, img: reactImage, stat: '' },
-    { id: 5, img: reactImage, stat: '' },
-    { id: 6, img: figmaImage, stat: '' },
-    { id: 6, img: figmaImage, stat: '' },
-    { id: 7, img: javaImage, stat: '' },
-    { id: 7, img: javaImage, stat: '' },
-    { id: 8, img: pythonImage, stat: '' },
-    { id: 8, img: pythonImage, stat: '' },
+    { id: 1, img: htmlImage, stat: '', pairClass: 'pair-1' },
+    { id: 2, img: htmlImage, stat: '', pairClass: 'pair-1' },
+    { id: 3, img: cssImage, stat: '', pairClass: 'pair-2' },
+    { id: 4, img: cssImage, stat: '', pairClass: 'pair-2' }, 
+    { id: 5, img: jsImage, stat: '', pairClass: 'pair-3' },
+    { id: 6, img: jsImage, stat: '', pairClass: 'pair-3' },
+    { id: 7, img: sassImage, stat: '', pairClass: 'pair-4' },
+    { id: 8, img: sassImage, stat: '',  pairClass: 'pair-4' },
+    { id: 9, img: reactImage, stat: '', pairClass: 'pair-5' },
+    { id: 10, img: reactImage, stat: '', pairClass: 'pair-5' },
+    { id: 11, img: figmaImage, stat: '', pairClass: 'pair-6' },
+    { id: 12, img: figmaImage, stat: '', pairClass: 'pair-6' },
+    { id: 13, img: javaImage, stat: '', pairClass: 'pair-7' },
+    { id: 14, img: javaImage, stat: '', pairClass: 'pair-7' },
+    { id: 15, img: pythonImage, stat: '', pairClass: 'pair-8' },
+    { id: 16, img: pythonImage, stat: '' , pairClass: 'pair-8'},
   ];
 
   return <Cards itemImgs={itemsImg} />;

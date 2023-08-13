ce43cb6d6c48fb4b9d5d98b6f8a1c327ea700f24
jovanatrajcheska <trajceskaj55@gmail.com>
2023-08-14 00:04:40 +0200

    Changes in tests for Cards component

--- src/tests/Cards.test.js
+++ src/tests/Cards.test.js
@@ -1,45 +1,39 @@
-import React from 'react';
+import React from "react";
 import { render, fireEvent } from "@testing-library/react";
-import Cards from '../components/Cards';
+import Cards from "../screens/Cards";
+import CardsProp from "../components/CardsProp";
 
+describe("Cards Component", () => {
+  // active card
+  test("checks if clicked card has class active", () => {
+    const { container } = render(<CardsProp />);
+    const cards = container.querySelectorAll(".card");
 
-describe('Cards Component', () => {
+    fireEvent.click(cards[7]);
+    expect(cards[7]).toHaveClass("active");
+  });
 
+  // is not active
+  test("check if another card is not active", () => {
+    const { container } = render(<CardsProp />);
+    const cards = container.querySelectorAll(".card");
 
-    // checking if there are 16 cards
-    test('16 cards', () => {
-        const { container } = render(<Cards />);
-        const cards = container.querySelectorAll('.card');
-        expect(cards.length).toBe(16);
-    });
+    // click on card
+    fireEvent.click(cards[3]);
+    expect(cards[3]).toHaveClass("active");
 
+    // check if all other cards are not active
+    for (let i = 0; i < cards.length; i++) {
+      if (i != 3) {
+        expect(cards[i]).not.toHaveClass("active");
+      }
+    }
+  });
 
-    // active card 
-    test('checks if clicked card has class active', () => {
-        const { container } = render(<Cards />);
-        const cards = container.querySelectorAll('.card');
+  test("checks if Start Over button exists", () => {
+    const { getByText } = render(<Cards itemImgs={[]} />);
+    const startOverButton = getByText("Start Over");
 
-        fireEvent.click(cards[7]);
-        expect(cards[7]).toHaveClass('active');
-    })
-
-    // is not active
-    test('check if another card is not active', () =>{
-        const { container } = render(<Cards/>)
-        const cards = container.querySelectorAll('.card')
-        
-
-        // click on card 
-        fireEvent.click(cards[3]);
-        expect(cards[3]).toHaveClass('active');
-
-        // check if all other cards are not active
-        for(let i = 0; i < cards.length; i++){
-                if(i != 3){
-                    expect(cards[i]).not.toHaveClass('active');    
-                }
-        }
-        
-
-    })
+    expect(startOverButton).toBeInTheDocument();
+  });
 });
